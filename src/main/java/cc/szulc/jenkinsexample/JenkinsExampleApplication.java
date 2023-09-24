07a1694b1b6145859849d2ad35080f88f6b4385a
Jacek Szulc <jacek@szulc.cc>
2023-09-24 13:25:52 +0200

    Initial commit

---
+++ src/main/java/cc/szulc/jenkinsexample/JenkinsExampleApplication.java
@@ -0,0 +1,21 @@
+package cc.szulc.jenkinsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsExampleApplication.class, args);
+    }
+
+    @GetMapping
+    public String get() {
+        return "Hello world";
+    }
+
+}
